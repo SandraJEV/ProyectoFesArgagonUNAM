57e47a59d6b18b75bb43fd294f404e67c66d7163
SandraJEV <piko.soune10@gmail.com>
2025-07-17 23:07:43 -0600

    Add conexion with Azure

---
+++ Backend/WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Role> Roles => Set<Role>();
+        public DbSet<Area> Areas => Set<Area>();
+    }
+}
--- Backend/WebAPI/Program.cs
+++ Backend/WebAPI/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data; // Donde tengas definido tu AppDbContext
+using System;
 using WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<WebAPI.Data.DbConnectionFactory>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<RoleService>();
