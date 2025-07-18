3017ef85c9035304c7117b0b85049c0ef80d19c7
SandraJEV <piko.soune10@gmail.com>
2025-07-18 01:04:17 -0600

    delete some files those are not necessary for now

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.cjs
-module.exports = {
-  plugins: [
-    require('tailwindcss'),  // Usa Tailwind CSS como un plugin
-    require('autoprefixer'),  // Asegúrate de incluir Autoprefixer también
-  ],
-};
